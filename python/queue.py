3adfccf0e3b28ac984a9a78aad968fb981c7c612
Angelika Jarosz <angelikajarosz@gmail.com>
2015-03-30 18:17:32 -0400

    simulate hot potato game with queue

--- python/queue.py
+++ python/queue.py
@@ -17,3 +17,21 @@ class Queue:
 
     def size(self):
         return len(self.items)
+
+
+# Simulate a game of hot potato using a queue
+
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size() > 1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
+
+print(hotPotato(['Guin', 'Pancake', 'Otter', 'Twinkie'], 3))
