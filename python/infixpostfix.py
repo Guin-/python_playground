95ab4f9abca14d8ab13000577402c86b38505ca9
Angelika Jarosz <angelikajarosz@gmail.com>
2015-03-30 16:52:53 -0400

    infix to postfix conversion and postfix evaluation

---
+++ python/infixpostfix.py
@@ -0,0 +1,88 @@
+# Converting infix to postfix
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+
+def infixToPostfix(expression):
+    prec ={}
+    prec['*'] = 3
+    prec['/'] = 3
+    prec['+'] = 2
+    prec['-'] = 2
+    prec['('] = 1
+
+    opStack = Stack()
+    postfixlist = []
+    infixlist = expression.split()
+
+    for token in infixlist:
+        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfixlist.append(token)
+        elif token == "(":
+            opStack.push(token)
+        elif token == ")":
+            topToken = opStack.pop()
+            while topToken != "(":
+                postfixlist.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and \
+                   (prec[opStack.peek()] >= prec[token]):
+                    postfixlist.append(opStack.pop())
+            opStack.push(token)
+
+    while not opStack.isEmpty():
+        postfixlist.append(opStack.pop())
+    return ' '.join(postfixlist)
+
+print(infixToPostfix("A * B + C * D"))
+print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+
+
+# Evaluating a postfix expression
+
+def postfixEval(postfixExpr):
+    operandStack = Stack()
+    tokenList = postfixExpr.split()
+
+    for token in tokenList:
+        if token in "0123456789":
+            operandStack.push(int(token))
+        else:
+            operand2 = operandStack.pop()
+            operand1 = operandStack.pop()
+            result = doMath(token, operand1, operand2)
+            operandStack.push(result)
+    return operandStack.pop()
+
+
+def doMath(op, op1, op2):
+    if op == "*":
+        return op1 * op2
+    elif op == "/":
+        return op1 / op2
+    elif op == "+":
+        return op1 + op2
+    else:
+        return op1 - op2
+
+print(postfixEval('7 8 + 3 2 + /'))
+
