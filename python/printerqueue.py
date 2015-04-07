0fc315db0565f1ae1438d7298c43df3a533bf29a
Angelika Jarosz <angelikajarosz@gmail.com>
2015-04-07 17:17:37 -0400

    simulation of printer task queue

--- python/printerqueue.py
+++ python/printerqueue.py
@@ -1,5 +1,7 @@
 # Simulate a printers printing queue
 
+import random
+
 class Queue:
 
     def __init__(self):
@@ -37,16 +39,57 @@ class Printer:
         else:
             return False
 
+    def startNext(self, newtask):
+        self.currentTask = newtask
+        self.timeRemaining = newtask.getPages() * 60/self.pagerate
+
 
 
 class Task:
 
-    def __init__(self):
-        pass
+    def __init__(self, time):
+        self.timestamp = time
+        self.pages = random.randrange(1, 21)
+
+    def getStamp(self):
+        return self.timestamp
+
+    def getPages(self):
+        return self.pages
+
+    def waitTime(self, currentTime):
+        return currentTime - self.timestamp
 
 
-class PrintQueue:
+def simulation(numSeconds, pagesPerMinute):
+    labPrinter = Printer(pagesPerMinute)
+    printQueue = Queue()
+    waitingtimes = []
 
-    def __init__(self):
-        self.tasktimestamp = timestamp
+    for currentSecond in range(numSeconds):
+
+        if newPrintTask():
+            task = Task(currentSecond)
+            printQueue.enqueue(task)
+
+        if (not labPrinter.busy()) and (not printQueue.isEmpty()):
+            nexttask = printQueue.dequeue()
+            waitingtimes.append(nexttask.waitTime(currentSecond))
+            labPrinter.startNext(nexttask)
+
+        labPrinter.tick()
+    averageWait = sum(waitingtimes)/len(waitingtimes)
+    print("Average Wait %6.2f secs %3d tasks remaining."%(averageWait, printQueue.size()))
+
+
+def newPrintTask():
+    num = random.randrange(1,181)
+    if num == 180:
+        return True
+
+for i in range(10):
+    simulation(3600, 5)
+
+
+
 
